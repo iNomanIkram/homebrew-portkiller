4872add1811e1ab3b685dd22d7bc797c9bde22cb
Noman Ikram <nomanikram0@icloud.com>
2026-03-17 18:56:58 +0500

    Update version number

--- portkiller.rb
+++ portkiller.rb
@@ -2,7 +2,7 @@ class portkiller < Formula
   desc "Advanced port process manager"
   homepage "https://github.com/inomanikram/homebrew-portkiller"
   version "1.0.1"
-  url "https://github.com/iNomanIkram/homebrew-portkiller/archive/refs/tags/v1.0.1.tar.gz"
+  url "https://github.com/iNomanIkram/homebrew-portkiller/archive/refs/tags/v1.0.2.tar.gz"
   sha256 "2986d739805549d9dc583731fa7b11f8f1bb7e780a3043e51247654a4fdaf578"
   license "MIT"
 
