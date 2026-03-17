2ce2cb64f95f699eac71ffda0e1a4a82fd6401da
Noman Ikram <nomanikram0@icloud.com>
2026-03-17 19:13:15 +0500

    Update sha

--- portkiller.rb
+++ portkiller.rb
@@ -3,7 +3,7 @@ class Portkiller < Formula
   homepage "https://github.com/inomanikram/homebrew-portkiller"
   version "1.0.3"
   url "https://github.com/iNomanIkram/homebrew-portkiller/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "ee1d15c33d95300096a0409fb806fee4d5e2021154bc575f223bb4f305712190"
+  sha256 "04de70e66f2e83c7eae428f13b709e90b113c51121593b6a4c36a0dae1a89b10"
   license "MIT"
 
   def install
